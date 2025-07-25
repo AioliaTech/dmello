3ba444d071e0f5b8c84ee787194ab0c88a7749fc
AioliaTech <aioliatechrs@gmail.com>
2025-07-25 16:14:20 -0300

    Update xml_fetcher.py

--- xml_fetcher.py
+++ xml_fetcher.py
@@ -249,7 +249,8 @@ class AutoconfParser(BaseParser):
             categoria_final = definir_categoria_veiculo(modelo_veiculo, opcionais_veiculo)
 
             parsed = self.normalize_vehicle({
-                "id": v.get("ID"), "tipo": ("carro" if v.get("CATEGORY") == "carros" else "moto" if v.get("CATEGORY") == "motos" else v.get("CATEGORY")), "titulo": None, "versao": v.get("VERSION"),
+                "id": v.get("ID"), "tipo": ("carro" if v.get("CATEGORY") == "carros" else "moto" if v.get("CATEGORY") == "motos" else v.get("CATEGORY")), "titulo": None, 
+                "titulo": None, "versao": (' '.join(re.sub(r'\b(\d\.\d|4x[0-4]|\d+v|diesel|flex|gasolina|manual|automático|4p)\b', '', v.get('VERSION', ''), flags=re.IGNORECASE).split()).strip()) if v.get("VERSION") else None,
                 "marca": v.get("MAKE"), "modelo": modelo_veiculo, "ano": v.get("YEAR"), "ano_fabricacao": v.get("FABRIC_YEAR"),
                 "km": v.get("MILEAGE"), "cor": v.get("COLOR"), "combustivel": v.get("FUEL"),
                 "cambio": v.get("gear") or v.get("GEAR"), "motor": v.get("MOTOR"), "portas": v.get("DOORS"),
